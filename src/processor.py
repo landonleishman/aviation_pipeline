4e88091bcee08c75883c2b69e874e028128bd354
landonleishman <landon.leishman@usu.edu>
2026-02-19 13:43:33 -0700

    fixing icoa24 columns, and implementing null values

--- src/processor.py
+++ src/processor.py
@@ -16,7 +16,7 @@ airplanes_df = spark.read.csv("data/reference/aircraft-database.csv", header=Tru
 
 
 
-#simple transformations
+#simple transformations- mapping iso_region to state
 airports_df = airports_df.withColumn("state", substring(col("iso_region"), length(col("iso_region")) - 1, 2))
 
 #renaming columns for clarity
@@ -35,6 +35,7 @@ airplanes_df = airplanes_df.withColumnRenamed("'icao24'", "icao24") \
                              .withColumnRenamed("'owner'", "owner") \
                              .withColumnRenamed("'typecode'", "typecode")
 
+
 #trimming down to basic schema and pertinent records
 refined_airports_df = airports_df.select("iata_code", "airport_name", "airport_type", "country","state" ,"city", "latitude", "longitude")
 target_types = ["large_airport", "medium_airport", "small_airport"]
@@ -42,8 +43,34 @@ refined_airports_df = refined_airports_df.filter((col("type").isin(target_types)
 refined_airports_df = refined_airports_df.filter(col("iata_code").isNotNull())
 refined_airports_df = refined_airports_df.filter(col("country") == "US")
 
-#airplanes df cleaning
+
+#-------airplanes df trimming-----------
 airplanes_df = airplanes_df.select("icao24", "timestamp", "manufacturer", "model", "category", "owner", "typecode")
+
+
+#-------airplanes df cleaning----------- 
+airplanes_df = airplanes_df.withColumn(
+    "icao24", F.lpad(F.lower(F.trim(F.col("icao24"))), 6, "0")) #getting rid of whitespace, converting to lowercase, and padding with zeros to ensure 6 characters
+
+airplanes_df = airplanes_df.withColumn(
+    "timestamp", 
+    F.expr("try_cast(regexp_replace(timestamp, \"['\\\"]\", '') as timestamp)")) # getting rid of quotes and converting to timestamp
+
+col_names = ['manufacturer', 'model', 'category', 'owner', 'typecode']
+for col_name in col_names:
+    airplanes_df = airplanes_df.withColumn(
+        col_name,
+        F.when(
+            (F.trim(F.col(col_name)) == "") | 
+            (F.trim(F.col(col_name)) == '"') | 
+            (F.trim(F.col(col_name)) == "''"), 
+            None
+        ).otherwise(F.col(col_name)))
+
+#-------fixing data types-----------    
+airplanes_df = airplanes_df.withColumn("timestamp", F.col("timestamp").cast("timestamp")) #casting timestamp to a timestamp data type
+
+
 
 #inserting
 db_url = "jdbc:postgresql://localhost:5432/aviation_pipeline"
